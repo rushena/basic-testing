3681d330b9fdb6b930fd0cbaddc98e64009c6d0f
Rushen Adjigeldieva <rushena2706@gmail.com>
2024-02-10 15:25:36 +0500

    feat: add snapchot node tests solution

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,23 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
+
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const simpleArr = [1];
+    const res = generateLinkedList(simpleArr);
+
+    expect(res).toStrictEqual({
+      value: 1,
+      next: {
+        value: null,
+        next: null
+      }
+    });
   });
 
-  // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const arr = [1, 2, 3, 'a', 'b', 'c'];
+    const res = generateLinkedList(arr);
+    expect(res).toMatchSnapshot();
   });
 });
